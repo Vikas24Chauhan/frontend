6f9af95e623b3767281c147e0db28886709d5a9f
Vikas24Chauhan <vikas24chauhan@gmail.com>
2026-07-27 10:18:40 +0530

    routing setup

---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+import Login from "../pages/auth/Login";
+import Register from "../pages/auth/Register";
+import Dashboard from "../pages/dashboard/Dashboard";
+import Users from "../pages/users/Users";
+import NotFound from "../pages/NotFound";
+
+import MainLayout from "../layouts/MainLayout";
+import AuthLayout from "../layouts/AuthLayout";
+
+function AppRoutes() {
+  return (
+    <Routes>
+      {/* Authentication */}
+      <Route element={<AuthLayout />}>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Route>
+
+      {/* Main App */}
+      <Route element={<MainLayout />}>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/users" element={<Users />} />
+      </Route>
+
+      {/* 404 */}
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+}
+
+export default AppRoutes;
