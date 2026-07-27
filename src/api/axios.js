0399aac70d1659ffd2fc726afaff9d14f3d33372
Vikas24Chauhan <vikas24chauhan@gmail.com>
2026-07-27 11:50:01 +0530

    login api integration with backend done

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
-  headers: {
-    "Content-Type": "application/json",
-  },
+  // baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "http://localhost:5000",
+  // headers: {
+  //   "Content-Type": "application/json",
+  // },
 });
 
 export default api;
--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,10 +1,77 @@
-import React from "react";
+import { useState } from "react";
+import { loginUser } from "../../services/authService";
+import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
 
 function Login() {
+  const navigate = useNavigate();
+
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await loginUser(formData);
+
+      console.log("Login Response:", res);
+
+      // Store JWT token
+      localStorage.setItem("token", res.token);
+
+      // Optional: Store user information
+      localStorage.setItem("user", JSON.stringify(res.user));
+
+      toast.success(res.message);
+
+      navigate("/");
+    } catch (error) {
+      console.error(error);
+
+      toast.error(
+        error.response?.data?.message || error.message || "Login failed",
+      );
+    }
+  };
+
   return (
-    <div>
-      <h1>Login Page</h1>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <h1>Login</h1>
+
+      <input
+        type="email"
+        name="email"
+        placeholder="Email"
+        value={formData.email}
+        onChange={handleChange}
+      />
+
+      <br />
+      <br />
+
+      <input
+        type="password"
+        name="password"
+        placeholder="Password"
+        value={formData.password}
+        onChange={handleChange}
+      />
+
+      <br />
+      <br />
+
+      <button type="submit">Login</button>
+    </form>
   );
 }
 
